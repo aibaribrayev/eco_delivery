1a414dd235f5b80a3fd431619386a02be5c4660f
Almaz <63015341+Morr231@users.noreply.github.com>
2023-01-20 20:28:30 +0600

    added place api

--- client/src/modules/SearchAddress/SearchAddress.js
+++ client/src/modules/SearchAddress/SearchAddress.js
@@ -12,6 +12,16 @@ const SearchAddress = () => {
     const [addressFound, setAddressFound] = useState(false);
     const [items, setItems] = useState([]);
 
+    const [placeRef, setPlaceRef] = useState();
+
+    const getCurrentPlace = () => {
+        if (placeRef.current.value === "") return;
+
+        console.log(placeRef.current.value);
+
+        console.log(window.google.maps.places);
+    };
+
     return (
         <div className="search-address">
             <div className="search-address__header">Введите Адрес</div>
@@ -20,13 +30,17 @@ const SearchAddress = () => {
                     <Input
                         placeholder="Адрес доставки"
                         style={{ marginBottom: "20px" }}
+                        setRef={setPlaceRef}
                     />
                 </Autocomplete>
                 {!addressFound ? (
                     <div className="search-address__form__el search-address__form__el_js-c">
                         <Button
                             text="Поиск"
-                            action={() => setAddressFound(true)}
+                            action={() => {
+                                setAddressFound(true);
+                                getCurrentPlace();
+                            }}
                         />
                     </div>
                 ) : (
